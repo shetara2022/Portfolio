e66173b6fd4bfbe080830bfe60515aa17a9b5450
shetara2022 <smithshetarall@gmail.com>
2023-02-25 10:02:02 -0500

    first commit with component pages created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Experience from './components/Experiences/Experience';
+import Projects from './components/Projects/Projects';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+
+        </Routes>
+      </Router>
+      <h1>Home</h1>
+
     </div>
   );
 }
