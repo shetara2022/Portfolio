4102137b54618224023c94b163cd77c59b0436c6
shetara2022 <smithshetarall@gmail.com>
2023-02-25 16:24:52 -0500

    added react simple typewriter text to home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Experience from './pages/Experiences/Experience';
 import Projects from './pages/Projects/Projects';
 import Home from './pages/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -14,10 +15,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
-
         </Routes>
       </Router>
-
+      <Footer />
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {useTypewriter, Cursor} from 'react-simple-typewriter'
 
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ['Fullstack Developer', 'Educator', 'Team Player'],
+    loop: {},
+  });
   return (
-    <div></div>
+    <>
+      <div className='home'>
+        <h1 style={{margin: '300px', color: 'white', justifyContent: 'center'}}>
+          I'm a {''}
+          <span style={{fontWeight: '500', color: 'red'}}>
+            {text}
+          </span>
+          <Cursor />
+        </h1>
+
+    </div>
+      <div className='about'>
+
+      </div>
+      <div className='skills'>
+
+      </div>
+    </>
   )
 }
 
